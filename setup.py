f9f53a953c826a72055fac92584c68eafd3f768d
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-10-22 15:02:25 -0300

    change version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='storyblok_richtext',
-    version='0.0.1',
+    version='0.1.0',
     description='Storyblok Python library for Richtext component',
     author='Emanuel Souza',
     author_email='eg@storyblok.com',
